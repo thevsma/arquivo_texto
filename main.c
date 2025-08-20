bb8cf2ea49c3373585a9ff2697f030862e220b75
Matheus Vieira <matheus.vieiraprates@gmail.com>
2025-08-19 22:01:30 -0300

    final commit

--- main.c
+++ main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <unistd.h>
 
 int main() {
     int total_char = 0, total_linhas = 0, total_palavras = 0;
 
     FILE* fp;
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    printf("Diretório atual: %s\n", cwd);
     fp = fopen("../entrada.txt", "r");
 
     if (fp == NULL) {
@@ -17,10 +13,13 @@ int main() {
         exit(1);
     }
 
-    int ch;
+    int ch, prev = 0;
     int in_word = 0;
     while ((ch = fgetc(fp)) != EOF) {
         total_char++;
+        if (ch == '\n') {
+            total_linhas++;
+        }
 
         if (isspace(ch)) {
             in_word = 0;
@@ -28,10 +27,11 @@ int main() {
             in_word = 1;
             total_palavras++;
         }
+        prev = ch;
+    }
 
-        if (ch == '\n') {
-            total_linhas++;
-        }
+    if (total_char > 0 && prev != '\n') {
+        total_linhas++;
     }
 
     printf("total de char: %d\ntotal de linhas: %d\ntotal de palavras: %d\n", total_char, total_linhas, total_palavras);
